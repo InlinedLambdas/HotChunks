413445d4a7e1dda6eed4868a14ed3987b2abee0b
iceBear67 <icebear67@sfclub.cc>
2022-03-12 20:32:41 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HotChunks
+
+This project is intended to solve a [performant problem](https://github.com/iceBear67/AstralFlow/issues/45#issuecomment-1065874236) from AstralFlow.
---
+++ src/main/java/io/ib67/astralflow/hotchunks/HotChunks.java
@@ -0,0 +1,17 @@
+package io.ib67.astralflow.hotchunks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HotChunks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
